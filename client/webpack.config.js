ec88cbfbefdebf8aeb1914721e9489bc7c770d80
Arthur Siqueira <arthur.siq5@gmail.com>
2020-04-27 22:11:28 -0300

    criado loader para os css do projeto

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
             }
         ]
     },
